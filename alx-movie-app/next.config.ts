d11f40aaae3e27563d76f6c2178202b527a93b8f
dijez <khadijaahmad2k21@gmail.com>
2025-12-07 06:40:59 +0100

    update

--- alx-movie-app/next.config.ts
+++ alx-movie-app/next.config.ts
@@ -3,6 +3,16 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: true,
+  // for images 
+   images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "m.media-amazon.com",
+      },
+    ],
+  },
+  
 };
 
 export default nextConfig;
